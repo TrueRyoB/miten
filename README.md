d997368e8f21d3c16f74f00d01dd917dc64a34dc
TrueRyoB <trueryob@proton.me>
2026-04-18 16:15:56 -0500

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+1. Row Level Security設定
+```
+ALTER TABLE your_table ENABLE ROW LEVEL SECURITY;
+```
+
+
+その他
+- pnpm
+- supabaseでホスト / DB
+- next.jsでUI
+- 無料枠
---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+module.exports = {
+  allowedDevOrigins: ['10.35.36.100'],
+}
+
+export default nextConfig;

8ac2a17f968867212746149dedf5efce21f240d9
TrueRyoB <trueryob@proton.me>
2026-04-18 20:37:36 -0500

    feat: hello world

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,14 @@ import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
 
 export default async function Page() {
-  const cookieStore = await cookies()
-  const supabase = createClient(cookieStore)
+  // const cookieStore = await cookies()
+  // const supabase = createClient(cookieStore)
 
-  const { data: todos } = await supabase.from('todos').select()
+  // const { data: todos } = await supabase.from('todos').select()
 
   return (
-    <ul>
-      {todos?.map((todo) => (
-        <li key={todo.id}>{todo.name}</li>
-      ))}
-    </ul>
+    <main className="flex flex-1 flex-col items-center justify-center gap-4 p-8">
+      <h1 className="text-2xl font-semibold tracking-tight">Hello world</h1>
+    </main>
   )
 }
